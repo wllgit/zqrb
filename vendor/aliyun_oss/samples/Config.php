5383d0498ceaf56c05026799133edaf6147baf12
wllgit <2794976767@qq.com>
2018-12-16 16:48:14 +0800

    vendor

---
+++ vendor/aliyun_oss/samples/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Class Config
+ *
+ * Make configurations required by the sample.
+ * Users can run RunAll.php which runs all the samples after configuring Endpoint, AccessId, and AccessKey.
+ */
+final class Config
+{
+    const OSS_ACCESS_ID = 'dZwbJBSoG9OREtPi';
+    const OSS_ACCESS_KEY = '3ShvUa4w153SVkoEjGod8HG6IyCUIQ';
+    const OSS_ENDPOINT = 'http://oss-cn-hangzhou.aliyuncs.com';
+    const OSS_TEST_BUCKET = 'zhengquanrb';
+}
